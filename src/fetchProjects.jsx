89c0709f64e4424fb7f08fcac6d08ec31c047726
Megagig <megagigsoftwaresolutions.com>
2024-09-06 17:47:19 +0100

    Fetch Projects

--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -1,10 +1,41 @@
 import { createClient } from 'contentful';
+import { useState, useEffect } from 'react';
 const client = createClient({
   space: 'u4i7cbtd8oxq',
   environment: 'master',
   accessToken: 'NIob94qwXc22URVY_WrvanttfzdsILLl49RS5QToO4M',
 });
 
-client
-  .getEntries({ content_type: 'projects' })
-  .then((response) => console.log(response));
+export const useFetchProjects = () => {
+  const [loading, setLoading] = useState(true);
+  const [projects, setProjects] = useState([]);
+
+  // create a Function to invoke when the component mounts.
+  const getData = async () => {
+    try {
+      const response = await client.getEntries({ content_type: 'projects' });
+
+      const projects = response.items.map((item) => {
+        const { title, url, image } = item.fields;
+        const { id } = item.sys.id;
+        const img = image?.fields?.file?.url;
+        return {
+          title,
+          url,
+          id,
+          img,
+        }; // return the data in the desired format
+      });
+      setProjects(projects);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+  return { loading, projects };
+};
